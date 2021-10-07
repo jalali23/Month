3d9a436057aa2d2cf6295c2f14cabc0be3ec1a95
jalali23 <73940488+jalali23@users.noreply.github.com>
2021-10-07 20:22:29 +0330

    final changes

--- js/script.js
+++ js/script.js
@@ -2,18 +2,20 @@ $(document).ready(function(){
 		$('.responsive-menu-icon').click(function(){
 		var responsiveMenu = $('.responsive-menu');
 		responsiveMenu.show();
+		$('.profile-pic').hide();
 		responsiveMenu.animate({
 			right: 0
 		},400);
 		$('body').append('<div class="back-container"></div>');
-		$('.back-container').click(function(){
-			var responsiveMenu = $('.responsive-menu');
+		$('.back-container').click(function(){			
+			var responsiveMenu = $('.responsive-menu');	
 			responsiveMenu.animate({
 				right: '-220px'
 			},400,function(){
-				responsiveMenu.hide();
+				responsiveMenu.hide();	
+				$('.profile-pic').show();		
 			});
-			$(this).remove();
+			$(this).remove();			
 		});
 	});
 });
